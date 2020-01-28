9b5adff3de7adfef8bbf2d682f3970d4c3ce240e
David Asare <gohan.david11@gmail.com>
2020-01-28 16:31:10 +0000

    Updates

---
+++ precache-manifest.6ee708465a41e90d44d5cf44c7548488.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea180d59b3102e6038f063399244135",
+    "url": "/movies-series-app/index.html"
+  },
+  {
+    "revision": "715723702e4d50121347",
+    "url": "/movies-series-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6af8f49a12aa37375c2c",
+    "url": "/movies-series-app/static/css/main.5f8c7372.chunk.css"
+  },
+  {
+    "revision": "715723702e4d50121347",
+    "url": "/movies-series-app/static/js/2.58b964bb.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/movies-series-app/static/js/2.58b964bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6af8f49a12aa37375c2c",
+    "url": "/movies-series-app/static/js/main.e1f6bfc9.chunk.js"
+  },
+  {
+    "revision": "b1b264a6f39e9a6e355e",
+    "url": "/movies-series-app/static/js/runtime-main.7df74386.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/movies-series-app/static/media/spinner.26ef8bb4.gif"
+  }
+]);
